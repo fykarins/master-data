5f672f69778b64ad142eebd1b9ee5dec054cc252
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2024-01-10 10:41:27 +0700

    connect api

--- InfoRecordPage.js
+++ InfoRecordPage.js
@@ -41,9 +41,12 @@ export const InfoRecordPage = () => {
   const [materialDesc, setMaterialDesc] = useState("");
   const [material, setMaterial] = useState("");
   const [vendorCode, setVendorCode] = useState("");
+  const [valueNmbr, setValueNmbr] = useState(""); //buat deklarasi state
 
   const filterVendorCode =
     user.vendor_code === null ? vendorCode : user.vendor_code;
+  const filterPurOrg =
+    user.purch_org === null ? valueNmbr : user.purch_org;
 
   useEffect(() => {
     // Reset on first load
@@ -58,6 +61,7 @@ export const InfoRecordPage = () => {
       INFNR: infoRecord,
       MATNR: material,
       material_description: materialDesc,
+      purch_org: filterPurOrg, //valueNmbr, //parameter pembacaan u/ melakukan permintaan API
       pageNo: 1,
       pageSize: 10,
     };
@@ -79,10 +83,14 @@ export const InfoRecordPage = () => {
         response.payload.data.error === "10008" ||
         response.payload.data.error === "10009"
       ) {
+        // Corrected the syntax here
         const action = await showErrorDialog(response.payload.data.message);
         if (action.isConfirmed) await history.push("/logout");
       } else {
-        showErrorDialog(response.payload.data.message);
+        // Corrected the syntax here
+        const action = await showErrorDialog(response.payload.data.message);
+        if (action.isConfirmed) await history.push("/logout");
+        valueNmbr = action.payload.value; // Corrected the syntax here
         setOverlayLoading(false);
       }
     } catch (error) {
@@ -103,6 +111,7 @@ export const InfoRecordPage = () => {
         INFNR: infoRecord,
         MATNR: material,
         material_description: materialDesc,
+        purch_org: filterPurOrg, //valueNmbr,
         pageNo: page,
         pageSize: sizePerPage,
       };
@@ -114,10 +123,14 @@ export const InfoRecordPage = () => {
           response.payload.data.error === "10008" ||
           response.payload.data.error === "10009"
         ) {
+          // Corrected the syntax here
           const action = await showErrorDialog(response.payload.data.message);
           if (action.isConfirmed) await history.push("/logout");
         } else {
-          showErrorDialog(response.payload.data.message);
+          // Corrected the syntax here
+          const action = await showErrorDialog(response.payload.data.message);
+          if (action.isConfirmed) await history.push("/logout");
+          valueNmbr = action.payload.value; // Corrected the syntax here
           setOverlayLoading(false);
         }
       } catch (error) {
@@ -271,8 +284,42 @@ export const InfoRecordPage = () => {
                   />
                 </Col>
               </Form.Group>
+              
               <Form.Group as={Row}>
-                <Col sm={3}>
+                <Col sm={6}>
+                  {user.purch_org !== null && (
+                    <Form.Group as={Row} className="mt-3">
+                      <Form.Label column sm={6}>
+                        <b>Purchasing Organization</b>
+                      </Form.Label>
+                      <Col sm={6}>
+                        <Form.Control
+                          type="text"
+                          placeholder="Purchasing Organization"
+                          value={user.purch_org}
+                          disabled
+                        />
+                      </Col>
+                    </Form.Group>
+                  )}
+                  {user.purch_org === null && (
+                    <Form.Group as={Row} className="mt-3">
+                      <Form.Label column sm={6}>
+                        <b>Purchasing Organization</b>
+                      </Form.Label>
+                      <Col sm={6}>
+                        <Form.Control
+                          type="text"
+                          placeholder="Purchasing Organization"
+                          onChange={(e) => {
+                            setValueNmbr(e.target.value); 
+                          }}
+                          value={valueNmbr} 
+                          onKeyPress={handleKeyPress}
+                        />
+                      </Col>
+                    </Form.Group>
+                 )}
                   <Button className="btn btn-danger" onClick={handleSearch}>
                     Search
                   </Button>
